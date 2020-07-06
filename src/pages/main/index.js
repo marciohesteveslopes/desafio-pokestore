dccfef9306bf2ef1511757ce6c658bf7fdfe12af
marciohesteveslopes <63199498+marciohesteveslopes@users.noreply.github.com>
2020-07-05 23:46:30 -0300

    primeiroCommit

---
+++ src/pages/main/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+import './styles.css';
+
+export default class Main extends Component {
+    state = {
+        pokemons: []
+    };
+
+    componentDidMount() {
+        this.loadPokemons();
+    }
+
+    loadPokemons = async () => {
+        const response = await api.get('/pokemon')
+        //console.log({pokemons: response.data.results})
+
+        this.setState({pokemons: response.data.results})
+    }
+
+    render() {
+        const {pokemons} = this.state;
+        
+        return (
+            <div className="pokemon-list">
+                {pokemons.map(pokemon => (
+                    <article key={pokemon.id}>
+                        <strong>{pokemon.name}</strong>
+                        <p>{pokemon.url}</p>
+
+                        <a href="">Acessar</a>
+                        
+                    </article>
+                ))}
+            </div>
+        );
+    }
+}
